b3e5bc3ed6b359e441c45dabf3aac26a26eae87e
stat-jack <jackmac@statmac.local>
2017-05-19 16:14:16 +1200

    changes made to front end, images changing and mega menu implemented

--- front/public_html/main_site.js
+++ front/public_html/main_site.js
@@ -5,9 +5,67 @@
  */
 
 $(document).ready( function (e) {
+    
+    // Globals
+    var $img = $("#top-banner-images"), i = 0, speed = 300;
+
    
+    /* Load all our images for our top banner into this array */
+    var images = [
+        "images/1391275534_nithiin.gif",
+        "images/chictravelphotos-22june154-1326.jpg",
+        "images/stock-photo-200657659.jpg"
+    ];
    
+    
+    /* Set out images to all be the same size, regardless */
+    $img.width($(window).width());
+    var windowHeight = $(window).height();
+    $img.height(windowHeight * 0.4);
+    
+    /* controls the size of the drop down menu */
+    $('.mega-dropdown-menu').height(windowHeight * 0.4);
+    
+    
+    /* controls the size of the buttons fo the menu */
+    var dropdownButtonsWidth = ($(window).width() * 0.7) / 7;
+    $('.dropdown-toggle').width(dropdownButtonsWidth);
+
+    /*
+     * Controls the changing of images in our top-banner. Done at 
+     * set interval, currently 10secs... Changes to 30secs
+     */
+    window.setInterval(function() {
+        $img.fadeOut(speed, function() {
+            $img.attr("src", images[(++i % images.length)]);
+            $img.fadeIn(speed);
+        });
+    }, 10000);
    
+   /* Use this funciton to change the top banner pictures to 
+    * suit the category you are currently looking at, We will copy 
+    * this for the women and kids section aswell
+    */
+   $(".men-cat-items").hover(function () {
+       //var manImageName = '';
+       var image = $("#top-banner-images");
+       //image.attr('src', manImageName);
+       //image.fadeIn();
+   });
+   
+   /* This doesn't work yet...
+    * We want to control the opening and closing of our drop down menu
+    * in our top-header. When you figure this out, please fix this for me
+    */
+   $(".mega-dropdown").hover(function(){
+        // need to get this working somehows
+        var stuff = $(this).children('ul');
+                stuff.show(5000);
+        //        alert(stuff);
+    },
+        function(){
+            $(this).children('ul').hide("fast");
+	});
    
 }); 
 
