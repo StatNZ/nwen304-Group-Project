f40668ab8eba64127117bf8f46b3f0b83aad0d7c
monaruan <mona@ruan.co.nz>
2017-06-27 11:29:10 +1200

    fixed onclick

--- public/js/categories.js
+++ public/js/categories.js
@@ -2,6 +2,13 @@
  * Created by monaruan on 27/06/17.
  */
 
-function myFunction() {
-    document.getElementById("demo").innerHTML = "Hello World";
+function dispMenCas() {
+    document.getElementById("men-casual").style.display = "block";
+    document.getElementById("men-formal").style.display = "none";
+}
+
+
+function dispMenForm() {
+    document.getElementById("men-formal").style.display = "block";
+    document.getElementById("men-casual").style.display = "none";
 }
