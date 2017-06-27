443d910693855b0a0ab4e1a5af931363f4aedaf7
monaruan <mona@ruan.co.nz>
2017-06-27 12:24:33 +1200

    adding indivudual display for images

--- public/js/categories.js
+++ public/js/categories.js
@@ -5,10 +5,16 @@
 function dispMenCas() {
     document.getElementById("men-casual").style.display = "block";
     document.getElementById("men-formal").style.display = "none";
+    document.getElementById("men-casual-top1").style.display = "none";
 }
-
+function dispMenCas1() {
+    document.getElementById("men-casual").style.display = "none";
+    document.getElementById("men-formal").style.display = "none";
+    document.getElementById("men-casual-top1").style.display = "block";
+}
 
 function dispMenForm() {
     document.getElementById("men-formal").style.display = "block";
     document.getElementById("men-casual").style.display = "none";
+    document.getElementById("men-casual-top1").style.display = "none";
 }
