c99fdd0a1dbf8833c74b2d1ecb733bc49a0f565a
monaruan <mona@ruan.co.nz>
2017-07-02 16:59:37 +1200

    Created sub_category js file

---
+++ public/js/sub_category.js
@@ -0,0 +1,40 @@
+/**
+ * Created by monaruan on 2/07/17.
+ */
+
+function dispMenAll() {
+    document.getElementById("men-formal").style.display = "none";
+    document.getElementById("men-casual-top1").style.display = "none";
+    document.getElementById("men-casual").style.display = "none";
+    document.getElementById("men-shirt").style.display = "none";
+    document.getElementsByClassName("men-sec").style.display = "block";
+    document.getElementsByClassName("men-sec-shirt").style.display = "none";
+    //document.getElementById("men-pop").style.display = "block";
+
+}
+
+function dispMenCas() {
+    document.getElementById("men-casual").style.display = "block";
+    document.getElementById("men-formal").style.display = "none";
+    document.getElementById("men-casual-top1").style.display = "none";
+    //document.getElementById("men-pop").style.display = "none";
+    document.getElementsByClassName("men-sec-shirt").style.display = "block";
+    document.getElementsByClassName("men-sec").style.display = "none";
+}
+function dispMenCas1() {
+    document.getElementById("men-casual").style.display = "none";
+    document.getElementById("men-formal").style.display = "none";
+    document.getElementById("men-casual-top1").style.display = "block";
+    //document.getElementById("men-pop").style.display = "none";
+    document.getElementsByClassName("men-sec-shirt").style.display = "block";
+    document.getElementsByClassName("men-sec").style.display = "none";
+}
+
+function dispMenForm() {
+    document.getElementById("men-formal").style.display = "block";
+    document.getElementById("men-casual").style.display = "none";
+    document.getElementById("men-casual-top1").style.display = "none";
+    //document.getElementById("men-pop").style.display = "none";
+    document.getElementsByClassName("men-sec-shirt").style.display = "block";
+    document.getElementsByClassName("men-sec").style.display = "none";
+}
