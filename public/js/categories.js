266d2667a4e521556c2615241321770c4d398435
monaruan <mona@ruan.co.nz>
2017-07-02 18:05:16 +1200

    displaying items in category page

--- public/js/categories.js
+++ public/js/categories.js
@@ -24,13 +24,60 @@ $(document).ready(function () {
     $('#cat-gender').text(newHrf.replace('http://localhost:3000/category/', ' ').toUpperCase());
 
     function displayCategoryItems (rows) {
-         var i;
+         /*var i;
          for (i = 0; i < rows.length; i++){
-             alert(rows[i].name);
+             //alert(rows[i].name);
+             $('#item-name').text(rows[i].name);
+             $('#item-price').text(rows[i].price);
              $('#category-img').image(rows[i].imagesource);
-             $('#item-name').text(rows[i].Name);
-             $('#item-price').text(rows[i].Price)
-         }
+
+
+         }*/
+
+        var i;
+
+        for (i=0; i<rows.length; i++) {
+            var uuid = rows[i].itemid;
+            var name = rows[i].name;
+            var desc = rows[i].description;
+            var price = rows[i].price;
+            var image = rows[i].imagesource;
+
+
+            var product_disp = '' +
+                '<tr>' +
+                '    <td data-th="item">' +
+                '       <div id="gender-cat" class="gender-cas">' +
+                '           <div class="responsive">' +
+                '               <div class="gallery">' +
+                '                   <a href="#" >' +
+                '                       <img class="category-img" src="" alt="" width="300" height="200" >' +
+                '                   </a>'+
+                '               <div class="desc">' +
+                '                   <a class="item-name"></a>' +
+                '               </div>' +
+                '               <div class="desc">' +
+                '                    <a class="item-price"></a>' +
+                '               </div>' +
+                '           </div>' +
+                '       </div>' +
+
+
+                '       <div class="clearfix"></div>' +
+                '       </div>'+
+                '   </td>'+
+                '</tr>';
+
+            var disp = $(product_disp);
+            //disp.find('.kart-item-delete-btn').attr('name', uuid);
+            disp.find('.item-name').text(name);
+            disp.find('.category-img').attr('src', image);
+            //disp.find('.item-description').text(desc);
+            //disp.find('.nomargin').text(name);
+            disp.find('.item-price').text(price);
+
+
+        }
 
 
     }
