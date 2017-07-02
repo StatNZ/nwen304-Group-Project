1c501b32feb4f8e6c7838de69c9de180ac4cd9d9
monaruan <mona@ruan.co.nz>
2017-07-02 16:59:53 +1200

    Created sub_category js file

--- public/js/categories.js
+++ public/js/categories.js
@@ -1,42 +1,7 @@
 /**
  * Created by monaruan on 27/06/17.
  */
-function dispMenAll() {
-    document.getElementById("men-formal").style.display = "none";
-    document.getElementById("men-casual-top1").style.display = "none";
-    document.getElementById("men-casual").style.display = "none";
-    document.getElementById("men-shirt").style.display = "none";
-    document.getElementsByClassName("men-sec").style.display = "block";
-    document.getElementsByClassName("men-sec-shirt").style.display = "none";
-    //document.getElementById("men-pop").style.display = "block";
 
-}
-
-function dispMenCas() {
-    document.getElementById("men-casual").style.display = "block";
-    document.getElementById("men-formal").style.display = "none";
-    document.getElementById("men-casual-top1").style.display = "none";
-    //document.getElementById("men-pop").style.display = "none";
-    document.getElementsByClassName("men-sec-shirt").style.display = "block";
-    document.getElementsByClassName("men-sec").style.display = "none";
-}
-function dispMenCas1() {
-    document.getElementById("men-casual").style.display = "none";
-    document.getElementById("men-formal").style.display = "none";
-    document.getElementById("men-casual-top1").style.display = "block";
-    //document.getElementById("men-pop").style.display = "none";
-    document.getElementsByClassName("men-sec-shirt").style.display = "block";
-    document.getElementsByClassName("men-sec").style.display = "none";
-}
-
-function dispMenForm() {
-    document.getElementById("men-formal").style.display = "block";
-    document.getElementById("men-casual").style.display = "none";
-    document.getElementById("men-casual-top1").style.display = "none";
-    //document.getElementById("men-pop").style.display = "none";
-    document.getElementsByClassName("men-sec-shirt").style.display = "block";
-    document.getElementsByClassName("men-sec").style.display = "none";
-}
 
 // call to get information
 $(document).ready(function () {
@@ -53,7 +18,7 @@ $(document).ready(function () {
                 alert ('something gone wrong');
             }
 
-        }).then(displayCatItems);
+        }).then(displayCategoryItems);
     }
 
     $('#cat-men').text(newHrf.replace('http://localhost:3000/category/', ' ').toUpperCase());
